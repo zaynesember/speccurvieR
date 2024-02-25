c8a87348960fb81b157b60a4a59614934ab1439e
Zayne Sember <zsember@ucsd.edu>
2024-02-24 19:18:39 -0800

    Brainstorming

---
+++ R/testing.R
@@ -0,0 +1,10 @@
+load("data/bottles.RData")
+
+# Idea: Find variation in coefficient of interest across k-folds, plot variance of coefficient
+#       across model specifications
+plotCV <- function(folds, y, x, controls, data, family="linear", link=NULL,
+                   fixedEffects=NULL, progressBar=TRUE, parallel=FALSE,
+                   workers=2){
+
+
+}
